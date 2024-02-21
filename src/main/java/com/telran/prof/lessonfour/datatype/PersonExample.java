7c711a50bf49d94b51295c53cf565f5dd6f18224
IuriiAv <Yurasha22r22@>
2024-02-22 00:10:12 +0300

    add lesson four part three

---
+++ src/main/java/com/telran/prof/lessonfour/datatype/PersonExample.java
@@ -0,0 +1,18 @@
+package com.telran.prof.lessonfour.datatype;
+
+public class PersonExample {
+
+    public static void main(String[] args) {
+        Person alex = new Person("Alex");
+        System.out.println("Person before " + alex);
+
+        changePersonName(alex);
+
+        System.out.println("Person after " + alex);
+        alex = null;
+    }
+
+    private static void changePersonName(Person maxim) {
+        maxim.setName("Maxim");
+    }
+}
