56b868ca79e1312442f614d474c2ca0f7db0bb8c
IuriiAv <Yurasha22r22@>
2024-02-28 23:36:28 +0300

    add lesson six part one

---
+++ src/main/java/com/telran/prof/lessonsix/TestListThree.java
@@ -0,0 +1,28 @@
+package com.telran.prof.lessonsix;
+
+import com.telran.prof.lessontwo.exthree.A;
+
+import java.util.ArrayList;
+
+public class TestListThree {
+
+    public static void main(String[] args) {
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        ArrayList<Integer> integerArrayList = new ArrayList<>();
+
+        stringArrayList.add("Hello");
+        stringArrayList.add("World");
+        System.out.println(stringArrayList);
+
+        integerArrayList.add(5);
+        integerArrayList.add(10);
+        System.out.println(integerArrayList);
+
+        stringArrayList.remove("World");
+        System.out.println(stringArrayList);
+
+        integerArrayList.remove(Integer.valueOf(10));
+        System.out.println(integerArrayList);
+
+    }
+}
