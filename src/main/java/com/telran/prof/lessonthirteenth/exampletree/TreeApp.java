987d5a816055bcedbfd20febc59865cd86f5dd3e
IuriiAv <Yurasha22r22@>
2024-03-25 23:27:52 +0300

    add lesson 13 part five

---
+++ src/main/java/com/telran/prof/lessonthirteenth/exampletree/TreeApp.java
@@ -0,0 +1,33 @@
+package com.telran.prof.lessonthirteenth.exampletree;
+
+public class TreeApp {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(10, null, null);
+        TreeNode left = new TreeNode(7, null, null);
+        TreeNode right = new TreeNode(13, null, null);
+        root.setLeft(left);
+        root.setRight(right);
+
+        //    10
+        //   /  \
+        //  7   13
+
+        CustomBinaryTree customBinaryTree = new CustomBinaryTree();
+        customBinaryTree.add(7);
+        customBinaryTree.add(4);
+        customBinaryTree.add(8);
+        customBinaryTree.add(2);
+        customBinaryTree.add(5);
+        customBinaryTree.add(10);
+        customBinaryTree.add(12);
+        /*
+             7
+          4     8
+        2   5      10
+                      12
+
+         */
+
+    }
+}
