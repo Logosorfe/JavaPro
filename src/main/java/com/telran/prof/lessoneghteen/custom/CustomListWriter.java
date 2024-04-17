1629e193583b8b7346771a083086690768864a36
IuriiAv <Yurasha22r22@>
2024-04-17 23:10:00 +0300

    add lesson 18 part five

--- src/main/java/com/telran/prof/lessoneghteen/custom/CustomListWriter.java
+++ src/main/java/com/telran/prof/lessoneghteen/custom/CustomListWriter.java
@@ -1,20 +1,35 @@
 package com.telran.prof.lessoneghteen.custom;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
-import java.util.List;
 
 public class CustomListWriter extends Writer {
 
     private String path;
 
+    private FileWriter fileWriter;
+
     public CustomListWriter(String path) {
         this.path = path;
+        init(); // not good idea
+    }
+
+    private void init() {
+        try {
+            fileWriter = new FileWriter(path);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 
     @Override
     public void write(char[] cbuf, int off, int len) throws IOException {
-
+        for (int i = 0; i < len; i++) {
+            fileWriter.write(cbuf[i]);
+        }
+        fileWriter.write('\n');
+        fileWriter.flush();
     }
 
     @Override
@@ -24,6 +39,6 @@ public class CustomListWriter extends Writer {
 
     @Override
     public void close() throws IOException {
-
+        fileWriter.close();
     }
 }
--- src/main/java/com/telran/prof/lessoneghteen/custom/CustomListWriterExample.java
+++ src/main/java/com/telran/prof/lessoneghteen/custom/CustomListWriterExample.java
@@ -10,7 +10,8 @@ public class CustomListWriterExample {
 
     public static void main(String[] args) {
         List<String> strings = Arrays.asList("Hello", "World", "Java", "SQL");
-        String path = "";
+        String path = "/Volumes/Macintosh HD - Data 1/telran3010prof/src/main/java/com/telran/prof/lessoneghteen/outout.txt";
+
 
         Writer customListWriter = new CustomListWriter(path);
         strings.stream().forEach(s -> {
