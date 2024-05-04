de9d439a726fbfa90308f7c34b8319b7e1375f8a
lapan1ch <vitamin.lapickis@gmail.com>
2024-05-05 01:17:15 +0300

    Errors found & corrected

--- src/main/java/com/telran/prof/lessonfifteen/ExampleStreamOperator.java
+++ src/main/java/com/telran/prof/lessonfifteen/ExampleStreamOperator.java
@@ -1,8 +1,6 @@
 package com.telran.prof.lessonfifteen;
 
 public class ExampleStreamOperator {
-
-    public static void main(String[] args) {
         //Intermediate
         //filter - intermediate, use filter(use FI Predicate) , true -> go further , false - rejected
         //sorted - intermediate, sort stream
@@ -11,13 +9,11 @@ public class ExampleStreamOperator {
         //peek - intermediate, analog foreach - make action for each element , but return stream
         //peek - use FI Consumer
         //map - intermediate , convert object
-        //flatMap -
+        //flatMap - intermediate, convert elements to plural streams
 
 
         //Terminal
         //foreach - terminal, make action for each element of stream (use FI Consumer)
         //count - terminal,  count of elements in stream
         //collect - terminal, collect element of stream to new collection
-
-    }
 }
