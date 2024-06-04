e3222158a95416124868373b72af2dddc3f1e867
IuriiAv <Yurasha22r22@>
2024-06-04 23:06:05 +0300

    add lesson 28 part two

---
+++ src/main/java/com/telran/prof/lessontwentyeight/cyclicbarier/FunicularExample.java
@@ -0,0 +1,18 @@
+package com.telran.prof.lessontwentyeight.cyclicbarier;
+
+
+import java.util.concurrent.CyclicBarrier;
+
+public class FunicularExample {
+
+    public static void main(String[] args) throws InterruptedException {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(2, new Funicular());
+
+        String[] names = {"Roman", "Alena", "Anna", "Kira", "Leo", "Bob", "Jack", "Inna"};
+
+        for (String name : names) {
+            new Thread(new PersonInFunicular(name, cyclicBarrier)).start();
+            Thread.sleep(700);
+        }
+    }
+}
