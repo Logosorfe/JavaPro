950be9de8f86ad48c444147e073260c937c27d1f
Logosorfe <logosorfe@gmail.com>
2025-02-15 00:46:23 +0200

    Errors and corrections

--- src/main/java/com/telran/prof/springlessons/lesson30/springbootdemo/MyApp.java
+++ src/main/java/com/telran/prof/springlessons/lesson30/springbootdemo/MyApp.java
@@ -1,16 +1,16 @@
 package com.telran.prof.springlessons.lesson30.springbootdemo;
 
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-////SPRING, TOMCAT
-//
-////SPRING BOOT (SPRING+TOMCAT)
-//
-//@SpringBootApplication
-//public class MyApp {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(MyApp.class, args);
-//	}
-//}
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//SPRING, TOMCAT
+
+//SPRING BOOT (SPRING+TOMCAT)
+
+@SpringBootApplication
+public class MyApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApp.class, args);
+	}
+}
