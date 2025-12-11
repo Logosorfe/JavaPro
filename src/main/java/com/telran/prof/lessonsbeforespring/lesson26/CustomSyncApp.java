0f2e44a5a025ec37d8f49cdf60633565ab7ae8fc
Logosorfe <logosorfe@gmail.com>
2025-12-11 15:32:15 +0200

    Errors and corrections

--- src/main/java/com/telran/prof/lessonsbeforespring/lesson26/CustomSyncApp.java
+++ src/main/java/com/telran/prof/lessonsbeforespring/lesson26/CustomSyncApp.java
@@ -4,7 +4,8 @@ public class CustomSyncApp {
 
     public static void main(String[] args) {
         Object monitor = new Object();
-        Object o = new Object();
+        Object o = new Object(); //То есть сам замысел исчезает если не использовать общий ресурс монитор,
+        //объект 'о' не нужен
 
         CustomSync customSyncOne = new CustomSync(monitor);
         CustomSync customSyncTwo = new CustomSync(o);
